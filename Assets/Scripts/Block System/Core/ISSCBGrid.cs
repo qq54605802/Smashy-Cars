d01083a24e2e5e8d3e205917ed1c27d5bb2d0dc0
HaoYan Qi <ceo@spinachelectr.com>
2015-12-04 22:23:35 +0800

    Core Grid Structure

---
+++ Assets/Scripts/Block System/Core/ISSCBGrid.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISSCBGrid : Object {
+
+	protected int[] blocks;
+
+	protected ISSCBlockVector gridSize;
+
+	public ISSCBGrid(ISSCBlockVector size){
+		gridSize = size;
+
+		blocks = new int[gridSize.Length()];
+	}
+
+	public int SetBlock(ISSCBlockVector position, int blockID){
+		int xIndex = position.x;
+		int yIndex = position.y * gridSize.x;
+		int zIndex = position.z * gridSize.x * gridSize.y;
+
+		int previousID = blocks [xIndex + yIndex + zIndex];
+
+		blocks [xIndex + yIndex + zIndex] = blockID;
+
+		return previousID;
+	}
+
+	public int GetBlock(ISSCBlockVector position){
+		int xIndex = position.x;
+		int yIndex = position.y * gridSize.x;
+		int zIndex = position.z * gridSize.x * gridSize.y;
+		return blocks [xIndex + yIndex + zIndex];
+	}
+
+}
+
+[System.Serializable]
+public struct ISSCBlockVector{
+	public int x,y,z;
+
+	public ISSCBlockVector(int x, int y, int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public int Length(){
+		return x * y * z;
+	}
+}
---
+++ Assets/Scripts/Prototyping/Testing/Testing.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testing : MonoBehaviour {
+
+	public int testNumber;
+
+	void Start () {
+		ISSCBGrid grid = new ISSCBGrid (new ISSCBlockVector(10,10,10));
+
+
+
+		for (int i = 0; i < testNumber; i++) {
+
+			ISSCBlockVector pos = new ISSCBlockVector(Random.Range(0,9),Random.Range(0,9),Random.Range(0,9));
+			int number2Set = Random.Range(0,10000);
+			grid.SetBlock(pos,number2Set);
+			if(grid.GetBlock(pos) == number2Set){
+				Debug.Log("Test " + i.ToString() + "Passed !");
+			}else{
+				Debug.LogWarning("Test " + i.ToString() + "Failed...");
+			}
+		}
+
+	}
+}
